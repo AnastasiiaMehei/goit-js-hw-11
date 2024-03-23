5565acef86c6c56bdca8030f85f004a00e971306
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-23 16:37:56 +0200

    123

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from '../js';
 export const refs = {
   formEl: document.querySelector('form'),
   formCont: document.querySelector('form-container'),
   inputEl: document.querySelector('form-control'),
   ulEl: document.querySelector('gallery'),
 };
+
+const fetchUsersBtn = document.querySelector('.btn');
+fetchUsersBtn.addEventListener('click', () => {
+  fetchImages()
+    .then(images => renderUsers(images))
+    .catch(error => console.log(error));
+});
